7ca9d3f36d019ac6f2311c160ae24056707d7088
NiakhaichykVitaliy <2603hbo@gmail.com>
2024-11-28 11:47:13 +0300

    [HBO-3] Delete import FragmentTransaction

--- app/src/main/java/com/example/sample/MainActivity.java
+++ app/src/main/java/com/example/sample/MainActivity.java
@@ -6,7 +6,6 @@ import android.os.Bundle;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.fragment.app.FragmentTransaction;
 
 public class MainActivity extends AppCompatActivity {
 
