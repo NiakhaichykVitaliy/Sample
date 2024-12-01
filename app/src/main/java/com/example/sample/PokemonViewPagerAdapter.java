e1a330ff6b14b93c5f6eafcfcfc501672ce2fee1
Nehaychik Vitaliy <2603hbo@gmail.com>
2024-12-01 14:56:40 +0300

    [HBO-4] Edit ViewPagerAdapter

--- app/src/main/java/com/example/sample/PokemonViewPagerAdapter.java
+++ app/src/main/java/com/example/sample/PokemonViewPagerAdapter.java
@@ -5,11 +5,7 @@ import androidx.fragment.app.Fragment;
 import androidx.fragment.app.FragmentActivity;
 import androidx.viewpager2.adapter.FragmentStateAdapter;
 
-import java.util.ArrayList;
-import java.util.List;
-
 public class PokemonViewPagerAdapter extends FragmentStateAdapter {
-    List<Pokemon> pokemonList = new ArrayList<>();
 
     public PokemonViewPagerAdapter(FragmentActivity fragment) {
         super(fragment);
@@ -18,10 +14,13 @@ public class PokemonViewPagerAdapter extends FragmentStateAdapter {
     @NonNull
     @Override
     public Fragment createFragment(int position) {
-        if (position == 0) {
-            PokemonFragment.getInstance();
-        } else if (position == 1) {
-            PokemonFragment.getInstance();
+        switch (position) {
+            case 0:
+                PokemonFragment.getInstance();
+                break;
+            case 1:
+                PokemonFragment.getInstance();
+                break;
         }
         return PokemonFragment.getInstance();
     }
