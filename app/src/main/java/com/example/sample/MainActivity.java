b1c0279133f77dfd8289dc691e5bc43efd4ab17e
NiakhaichykVitaliy <2603hbo@gmail.com>
2024-12-05 09:13:40 +0300

    [HBO-3] Delete PokemonViewPagerActivity and replace all logic in MainActivity

--- app/src/main/java/com/example/sample/MainActivity.java
+++ app/src/main/java/com/example/sample/MainActivity.java
@@ -5,15 +5,41 @@ import android.content.Intent;
 import android.os.Bundle;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.viewpager2.widget.ViewPager2;
+
+import com.google.android.material.tabs.TabLayout;
+import com.google.android.material.tabs.TabLayoutMediator;
 
 public class MainActivity extends AppCompatActivity {
+    private ViewPager2 viewPager;
+    private TabLayout tabLayout;
+    private PokemonViewPagerAdapter viewPagerAdapter;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        setContentView(R.layout.viewpager_pokemon);
 
-        setContentView(R.layout.activity_main);
-        PokemonViewPagerActivity.startPokemonViewPagerActivity(this);
+        viewPager = findViewById(R.id.view_pager);
+        tabLayout = findViewById(R.id.tab_layout);
+
+        viewPagerAdapter = new PokemonViewPagerAdapter(this);
+        viewPager.setAdapter(viewPagerAdapter);
+
+        new TabLayoutMediator(tabLayout, viewPager, (tab, position) -> {
+            switch (position) {
+                case 0:
+                    tab.setText(R.string.pokemon_fragment_1);
+                    tab.setIcon(R.drawable.poke_ball);
+                    break;
+                case 1:
+                    tab.setText(R.string.pokemon_fragment_2);
+                    tab.setIcon(R.drawable.poke_ball);
+                    break;
+            }
+        }).attach();
+
+        viewPager.setUserInputEnabled(false);
     }
 
     static void startMainActivity(Context context) {
