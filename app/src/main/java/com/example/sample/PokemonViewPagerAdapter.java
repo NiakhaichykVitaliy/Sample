c58ccec4a3cb54d9cbbfaaa7794ee7d74f612beb
NiakhaichykVitaliy <2603hbo@gmail.com>
2024-12-10 17:04:16 +0300

    [HBO-5] Delete trash line

--- app/src/main/java/com/example/sample/PokemonViewPagerAdapter.java
+++ app/src/main/java/com/example/sample/PokemonViewPagerAdapter.java
@@ -5,7 +5,6 @@ import androidx.fragment.app.Fragment;
 import androidx.fragment.app.FragmentActivity;
 import androidx.viewpager2.adapter.FragmentStateAdapter;
 
-
 public class PokemonViewPagerAdapter extends FragmentStateAdapter {
 
     public PokemonViewPagerAdapter(FragmentActivity fragment) {
