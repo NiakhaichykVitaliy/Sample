d1f2c2a23fa192b2db5ad5e96ebecf66456e2ce0
NiakhaichykVitaliy <2603hbo@gmail.com>
2024-12-17 19:14:34 +0300

    [HBO-7] Edit gradle in PokemonModule

--- pokemon/build.gradle.kts
+++ pokemon/build.gradle.kts
@@ -7,22 +7,6 @@ android {
     namespace = "com.example.pokemon"
     compileSdk = 34
 
-    defaultConfig {
-        minSdk = 24
-
-        testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
-        consumerProguardFiles("consumer-rules.pro")
-    }
-
-    buildTypes {
-        release {
-            isMinifyEnabled = false
-            proguardFiles(
-                getDefaultProguardFile("proguard-android-optimize.txt"),
-                "proguard-rules.pro"
-            )
-        }
-    }
     compileOptions {
         sourceCompatibility = JavaVersion.VERSION_17
         targetCompatibility = JavaVersion.VERSION_17
@@ -33,17 +17,11 @@ android {
 }
 
 dependencies {
-    implementation (libs.fragment.ktx)
+    implementation(libs.fragment.ktx)
     implementation(libs.lifecycle.viewmodel.ktx)
     implementation(libs.lifecycle.livedata.ktx)
     implementation(libs.glide)
     implementation(libs.recyclerview)
     implementation(libs.appcompat)
     implementation(libs.material)
-    implementation(libs.activity)
-    implementation(libs.constraintlayout)
-    implementation(libs.core.ktx)
-    testImplementation(libs.junit)
-    androidTestImplementation(libs.ext.junit)
-    androidTestImplementation(libs.espresso.core)
 }
