9f27dc3f9aa45ca2006a5f18f9fffbfac92aac3d
Nehaychik Vitaliy <2603hbo@gmail.com>
2025-01-08 13:00:00 +0300

    [HBO-11] Connect profilemodule to app

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -37,6 +37,7 @@ android {
 }
 
 dependencies {
+    implementation(project(":profilemodule"))
     implementation(project(":pokemon"))
     implementation(libs.fragment.ktx)
     implementation(libs.lifecycle.viewmodel.ktx)
