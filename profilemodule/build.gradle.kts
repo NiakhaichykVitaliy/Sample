d5f5aca3194c4b5eafcff8ee602845a672a9edf6
Nehaychik Vitaliy <2603hbo@gmail.com>
2025-01-08 12:57:35 +0300

    [HBO-11] Create profilemodule

---
+++ profilemodule/build.gradle.kts
@@ -0,0 +1,23 @@
+plugins {
+    alias(libs.plugins.android.library)
+    alias(libs.plugins.jetbrains.kotlin.android)
+}
+
+android {
+    namespace = "com.example.profilemodule"
+    compileSdk = 34
+
+    compileOptions {
+        sourceCompatibility = JavaVersion.VERSION_17
+        targetCompatibility = JavaVersion.VERSION_17
+    }
+    kotlinOptions {
+        jvmTarget = "17"
+    }
+}
+
+dependencies {
+    implementation(libs.core.ktx)
+    implementation(libs.appcompat)
+    implementation(libs.material)
+}
