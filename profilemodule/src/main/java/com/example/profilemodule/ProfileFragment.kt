376c3c1b1f203a4d58e4eb02ecdfe3cccf7aaa7d
Nehaychik Vitaliy <2603hbo@gmail.com>
2025-01-09 18:48:10 +0300

    [HBO-11] Implementation of SharedPreferences through the core module

--- profilemodule/src/main/java/com/example/profilemodule/ProfileFragment.kt
+++ profilemodule/src/main/java/com/example/profilemodule/ProfileFragment.kt
@@ -1,10 +1,6 @@
 package com.example.profilemodule
 
-import android.content.Context
-import android.content.Intent
-import android.content.SharedPreferences
 import android.os.Bundle
-import android.provider.SyncStateContract.Constants
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
@@ -12,32 +8,41 @@ import android.widget.Button
 import android.widget.TextView
 import android.widget.Toast
 import androidx.fragment.app.Fragment
+import com.example.core.SharedPreferencesManager
 
 class ProfileFragment : Fragment() {
     private val userNameTextView: TextView by lazy { requireView().findViewById(R.id.username_text_view) }
     private val logoutButton: Button by lazy { requireView().findViewById(R.id.logout_button) }
+    private val loginActivityLauncher: LoginActivityLauncher by lazy {
+        requireActivity() as LoginActivityLauncher
+    }
 
     override fun onCreateView(
         inflater: LayoutInflater,
         container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        val view = inflater.inflate(R.layout.profile_fragment, container, false)
-        val sharedPreference: SharedPreferences
-        sharedPreference = requireActivity().getSharedPreferences("app_prefs", Context.MODE_PRIVATE)
-        val userName = sharedPreference.getString("username", null)
+        return inflater.inflate(R.layout.profile_fragment, container, false)
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        val sharedPreferencesManager = SharedPreferencesManager(requireContext())
+        val userName = sharedPreferencesManager.getUserName()
 
         if (userName != null) {
             userNameTextView.text = userName
         } else {
-            Toast.makeText(requireContext(), "No username found", Toast.LENGTH_SHORT).show()
+            Toast.makeText(
+                requireContext(),
+                getString(R.string.profile_no_username_found), Toast.LENGTH_SHORT
+            ).show()
         }
 
         logoutButton.setOnClickListener {
-
+            sharedPreferencesManager.removeUserName()
+            loginActivityLauncher.startLoginActivity()
         }
-
-        return view
     }
 
     companion object {
